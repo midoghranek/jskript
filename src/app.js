f64b2c050cded359315ae738f7c2e38d0ab13f94
midoghranek <midoghranek@hotmail.com>
2019-07-07 19:55:01 +0200

    front

--- src/app.js
+++ src/app.js
@@ -10,11 +10,8 @@ app.use(contactRouter);
 
 // NOTE Paths
 const publicDirPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates/views");
 
 // NOTE Handlebars and css
 app.use(express.static(publicDirPath));
-app.set("view engine", "hbs");
-app.set("views", viewsPath);
 
 module.exports = app;
