8ca76c6595302a462ddd5025354294c8c29112da
midoghranek <midoghranek@hotmail.com>
2019-07-08 19:07:05 +0200

    v0.0.1

---
+++ public/src/javascript/main.js
@@ -0,0 +1,9 @@
+// // Javascript
+// const input = document.querySelectorAll(".input-field");
+
+// input.addEventListener("mouseover", function() {
+//   this.classList.add("active");
+// });
+// input.addEventListener("mouseout", function() {
+//   this.classList.remove("active");
+// });
