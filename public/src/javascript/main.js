074d8510ef4bc182ac85e2834ff72ee1f0916f33
midoghranek <midoghranek@hotmail.com>
2019-07-08 23:25:07 +0200

    v0.0.3

--- public/src/javascript/main.js
+++ public/src/javascript/main.js
@@ -1,11 +1,23 @@
-// // Javascript
-// const input = document.querySelectorAll(".input-field input");
+// Javascript
+const input = document.querySelectorAll(
+  ".input-field input, input-field textarea"
+);
 
-// input.forEach(field => {
-//   field.addEventListener("focus", function() {
-//     this.parentElement.classList.add("active");
-//   });
-//   field.addEventListener("blur", function() {
-//     this.parentElement.classList.remove("active");
-//   });
-// });
+input.forEach(field => {
+  field.addEventListener("change", function() {
+    if (this.value !== "") {
+      this.parentElement.classList.add("active");
+    } else {
+      this.parentElement.classList.remove("active");
+    }
+  });
+
+  field.addEventListener("focus", function() {
+    this.parentElement.classList.add("active");
+  });
+  field.addEventListener("blur", function() {
+    if (this.value === "") {
+      this.parentElement.classList.remove("active");
+    }
+  });
+});
